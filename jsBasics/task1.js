fc652695e083b4edc1e99e4ce35d1c9b5a345d37
Andrey Pushai <andrey.pushai@gmail.com>
2025-02-04 13:43:05 +0100

    Lesson 4. Added task files with completed homework in jsBasics folder.

---
+++ jsBasics/task1.js
@@ -0,0 +1,13 @@
+
+let myString = "string";
+let myNumber = 123;
+let myBool = true;
+let myNull = null;
+let myUndefined = undefined;
+
+console.log(myString);
+console.log(myNumber);
+console.log(myBool);
+console.log(myNull);
+console.log(myUndefined);
+
---
+++ jsBasics/task4_2.js
@@ -0,0 +1,7 @@
+
+let length = 14244;
+let width = 24242;
+
+let area = length * width;
+console.log(area.toFixed(2));
+
