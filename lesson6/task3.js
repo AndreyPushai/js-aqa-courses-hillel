5c520e84256b6ee380da36752f8c1cb5fa29e70d
Andrey Pushai <andrey.pushai@gmail.com>
2025-02-13 09:47:29 +0100

    Updated lesson3
    Finished task 3

--- lesson6/task3.js
+++ lesson6/task3.js
@@ -1,4 +1,8 @@
 
-function checkOrder() {
+function checkOrder(available, ordered) {
+    if (available < ordered) return "Your order is too large, we don’t have enough goods";
+    if (ordered === 0) return "Your order is empty";
+    return "Your order is accepted";
 
 }
+
